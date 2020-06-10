0f08377287c8d19990969abdb7c39da3adae41d9
Cycloid <devops@cycloid.io>
2020-06-10 15:03:35 +0000

    Bump stacks

---
+++ infra-amazon-s3/jamestest1million/default/terraform/module-jamestest1million/config.tf
@@ -0,0 +1,41 @@
+"locals" = {
+  "tags_cycloid" = {
+    "cycloid.io" = "true"
+
+    "env" = "${var.env}"
+  }
+
+  "tags_stack" = {}
+}
+
+"provider" "openstack" {}
+
+"provider" "vault" {
+  "address" = "dadfadf2e324"
+
+  "token" = "ad34234"
+}
+
+"variable" "env" {
+  "default" = "[PLACEHOLDER]"
+
+  "type" = "string"
+}
+
+"variable" "network_id" {
+  "description" = "The ID of the network to attach the port resources to"
+
+  "type" = "string"
+}
+
+"variable" "project" {
+  "default" = "[PLACEHOLDER]"
+
+  "type" = "string"
+}
+
+"variable" "subnet_id" {
+  "description" = "Subnet in which to allocate an IP address"
+
+  "type" = "string"
+}
