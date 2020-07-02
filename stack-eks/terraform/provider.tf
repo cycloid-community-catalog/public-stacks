972b2420ed4db2a9f7bb58489e280a82ccd84d18
Cycloid <devops@cycloid.io>
2020-07-02 17:26:11 +0000

    Bump stacks

--- stack-eks/terraform/provider.tf
+++ stack-eks/terraform/provider.tf
@@ -4,17 +4,3 @@ provider "aws" {
   secret_key = var.secret_key
   region     = var.aws_region
 }
-
-# Kubernetes
-# In order to be able to connect to the EKS cluster
-# we are creating
-data "aws_eks_cluster_auth" "eks-cluster" {
-  name = module.eks.cluster_name
-}
-
-provider "kubernetes" {
-  host                   = module.eks.control_plane_endpoint
-  cluster_ca_certificate = base64decode(module.eks.control_plane_ca)
-  token                  = data.aws_eks_cluster_auth.eks-cluster.token
-  load_config_file       = false
-}
