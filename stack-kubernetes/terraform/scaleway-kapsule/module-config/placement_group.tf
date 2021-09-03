9a7b68a142b5193aeb3f04f089c49524f4f4ac42
Cycloid <devops@cycloid.io>
2021-09-03 07:53:15 +0000

    Bump stacks

--- stack-kubernetes/terraform/scaleway-kapsule/module-config/placement_group.tf
+++ stack-kubernetes/terraform/scaleway-kapsule/module-config/placement_group.tf
@@ -1,4 +1,4 @@
-resource "scaleway_instance_placement_group" "placement_group" {
+resource "scaleway_instance_placement_group" "($ environment $)" {
   name        = var.placement_group_name
   policy_type = "max_availability"
   policy_mode = "optional"
